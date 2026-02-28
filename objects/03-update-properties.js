85f3a63c4bcf0e82e2be19d1cf52965fca1c2244
Igor Ruban <ruban.89.ihor@gmail.com>
2026-02-28 21:46:09 +0200

    feat(objects): update object properties (#03)

---
+++ objects/03-update-properties.js
@@ -0,0 +1,15 @@
+// 🎯 Уровень 1 → Задача №3
+// Условие:
+// Дан объект product = {name: 'Laptop', price: 1000}.
+// Задание:
+
+// Увеличь price на 200
+// Добавь новое свойство inStock: true
+
+const product = { name: 'Laptop', price: 1000 };
+
+product.price += 200;
+product.inStock = true;
+
+console.log(product);
+

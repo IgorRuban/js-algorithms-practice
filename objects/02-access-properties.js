02a4d67d1ff162ec04430a34b0bd2f54fafa99ed
Igor Ruban <ruban.89.ihor@gmail.com>
2026-02-28 21:01:32 +0200

    feat(objects): access object properties (#02)

---
+++ objects/02-access-properties.js
@@ -0,0 +1,10 @@
+// 🎯 Уровень 1 → Задача №2
+// Условие:
+// Дан объект car = {brand: 'Toyota', model: 'Camry', year: 2020}.
+// Задание: Выведи brand через точку, а year через квадратные скобки.
+
+const car = { brand: 'Toyota', model: 'Camry', year: 2020 };
+
+console.log(car.brand);
+console.log(car['year']);
+

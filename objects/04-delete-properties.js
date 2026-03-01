0dd319f8194f2dfe04f841116d716dfbf1225016
Igor Ruban <ruban.89.ihor@gmail.com>
2026-03-01 21:23:35 +0200

    feat(objects): delete object properties (#04)

---
+++ objects/04-delete-properties.js
@@ -0,0 +1,9 @@
+// 🎯 Уровень 1 → Задача №4
+// Условие:
+// Дан объект person = {name: 'Anna', age: 30, password: '12345'}.
+// Задание: Удали свойство password.
+
+const person = { name: 'Anna', age: 30, password: '12345' };
+
+delete person.password
+console.log(person);
